4507d052f044ef84889805f4b50228f330880752
eakasak piboonkit <eakasak.pib@truedigital.com>
2022-04-25 11:08:13 +0700

    commit

---
+++ PerformanceReportXLineNoti/APISv.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static PerformanceReportXLineNoti.Program;
+
+namespace PerformanceReportXLineNoti
+{
+    public class APISv
+    {
+        public string _Authorization = "Bearer 2SkCQsMhBftwXDtQXbtmB8w2PauLnAb2X2JuraKK4GF";
+
+        public object NotiLine(string pMessage)
+        {
+            //  client.Timeout = -1;
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Post;
+            var client = new RestClient("https://notify-api.line.me/api/notify?message=" + pMessage);
+            request.AddHeader("Authorization", _Authorization);
+            request.AddHeader("Accept", "application/json");
+            //request.AddFile("imageFile", "D://Image.png");
+            var response = client.ExecuteAsync(request).Result;
+            //Console.WriteLine(response.Content);
+            return response.Content;
+        }
+        public object NotiLine(string pMessage, string imageFile)
+        {
+            //  client.Timeout = -1;
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Post;
+            var client = new RestClient("https://notify-api.line.me/api/notify?message=" + pMessage);
+            request.AddHeader("Authorization", _Authorization);
+            request.AddHeader("Accept", "application/json");
+            request.AddFile("imageFile", imageFile);
+            var response = client.ExecuteAsync(request).Result;
+            //Console.WriteLine(response.Content);
+            return response.Content;
+        }
+        public List<Fixture> GetFixtures()
+        {
+            var client = new RestClient("https://fantasy.premierleague.com/api/fixtures/");
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Get;
+            var response = client.ExecuteAsync(request).Result;
+            //Console.WriteLine(response.Content);
+            var rs  = JsonConvert.DeserializeObject<List<Fixture>>(response.Content);
+            return rs;
+
+        }
+        public string Gettoken()
+        {
+            var client = new RestClient("https://api.pure1.purestorage.com/oauth2/1.0/token");
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Post;
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Authorization", "Basic ZWFrYXNhay5waWI6Ykd2aE1wMlhXa3RrVzY2aGtjalYjJCU=");
+            request.AddParameter("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange");
+            request.AddParameter("subject_token", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwdXJlMTphcGlrZXk6ZVhjR3B1b0dpYzg4Q1lZYyIsImlhdCI6MTU0NjYyMDYxMzAyOCwiZXhwIjoxNTU2NjIwNjEzMDI4fQ.nDL6XHR8NqzuoHjSNLdcblkFUREZm2Z6Ki1pHl-zINwr7SAB-YqI6u0R5ETcXEed0z6cdu5UnvdhdAAJW5DSNJhTXT4q_3eCHn71Ruq6aHkIWuUz7ZdMYITktl4tyYpkBiCG6PClFWxARfJ5W0o10CTrhY4iKthdQH0UC4S_LQkzySARyFqu36S8tGIjKgv046tCHx2F4CAVoX38v5nEeYPYhyOz0rXMjKUOwPfNfhGqnCOjFGzCxGjzahZhrFk0B6aDEiOXUKLdxzcJiKefcxGaO3fdhJiSuD4RLYN9mWTVuRCS6OJyeaNkLwcDdOznjEA2_s6O1ELVSsMarwtqOA");
+            request.AddParameter("subject_token_type", "urn:ietf:params:oauth:token-type:jwt");
+            var response = client.ExecuteAsync(request).Result;
+            //Console.WriteLine(response.Content);
+            var rs = JsonConvert.DeserializeObject<AccessToken>(response.Content);
+            return rs.access_token;
+
+        }
+
+        public PureArray GetArrays()
+        {
+            var client = new RestClient("https://api.pure1.purestorage.com/api/1.0/arrays");
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Get;
+            request.AddHeader("Authorization", "Bearer " + Gettoken());
+            var response = client.ExecuteAsync(request).Result;
+            PureArray myDeserializedClass = JsonConvert.DeserializeObject<PureArray>(response.Content);
+            var rs = new List<PureItem>();
+            rs = myDeserializedClass.items;
+            return myDeserializedClass;
+
+        }
+
+        public object GetVolumes()
+        {
+            var client = new RestClient("https://api.pure1.purestorage.com/api/1.0/volumes");
+
+            var request = new RestRequest();
+            request.RequestFormat = DataFormat.Json;
+            request.Method = Method.Get;
+            request.AddHeader("Authorization", "Bearer " + Gettoken());
+            var response = client.ExecuteAsync(request).Result;
+            return response;
+
+        }
+        public class AccessToken
+        {
+            public string access_token { get; set; }
+            public string issued_token_type { get; set; }
+            public string token_type { get; set; }
+            public int expires_in { get; set; }
+        }
+        public class Fixture
+        {
+            public int code { get; set; }
+            public object @event { get; set; }
+            public bool finished { get; set; }
+            public bool finished_provisional { get; set; }
+            public int id { get; set; }
+            public string kickoff_time { get; set; }
+            public int minutes { get; set; }
+            public bool provisional_start_time { get; set; }
+            public object started { get; set; }
+            public int team_a { get; set; }
+            public object team_a_score { get; set; }
+            public int team_h { get; set; }
+            public object team_h_score { get; set; }
+            public List<object> stats { get; set; }
+            public int team_h_difficulty { get; set; }
+            public int team_a_difficulty { get; set; }
+            public int pulse_id { get; set; }
+        }
+        public class PureItem
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string model { get; set; }
+            public string os { get; set; }
+            public string version { get; set; }
+            public object _as_of { get; set; }
+        }
+
+        public class PureArray
+        {
+            public int total_item_count { get; set; }
+            public object continuation_token { get; set; }
+            public List<PureItem> items { get; set; }
+        }
+    }
+}
---
+++ PerformanceReportXLineNoti/Program.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using static PerformanceReportXLineNoti.APISv;
+
+namespace PerformanceReportXLineNoti
+{
+    class Program
+    {
+        static List<string> teams = new List<string>{"Arsenal", "Aston Villa", "Brentford", "Brighton", "Burnley", "Chelsea", "Crystal Palace",
+            "Everton", "Leeds", "Leicester", "Liverpool", "Man City", "Man Utd", "Newcastle", "Norwich City", "Southampton", "Tottenham", "Watford", "West Ham", "Wolves" };
+
+        static APISv _APISv = new APISv();
+        static SystemConfig _SystemConfig = new SystemConfig();
+        static DateTime beginko;
+        static List<DateTime> MacthDay = new List<DateTime>();
+        static void Main(string[] args)
+        {
+            SetTimer();
+            LoadConfig();
+            var curdate = DateTime.Now;
+            var curmacth = DateTime.Now;
+
+            var rs = _APISv.GetFixtures();
+
+            var rs2 = rs.Where(a => Convert.ToDateTime(a.kickoff_time).Date >= curdate).Take(1).ToList();
+
+            //var c = Convert.ToDateTime(rs2[0].kickoff_time);
+
+            foreach (var item in rs2)
+            {
+                var d = Convert.ToDateTime(item.kickoff_time);
+              
+                var h = teams[item.team_h - 1];
+                var a = teams[item.team_a - 1];
+                beginko = d.AddMinutes(-15);
+
+                var msg = d + " : " + h + " vs " + a;
+                msg += Environment.NewLine;
+                if (d > curmacth)
+                {
+                    msg += "rp1 : begin ko : " + beginko.ToString();
+                    msg += Environment.NewLine;
+                    MacthDay.Add(beginko);
+                    msg += "rp2 : " + beginko.AddMinutes(5).ToString();
+                    msg += Environment.NewLine;
+                    MacthDay.Add(beginko.AddMinutes(5));
+                    msg += "rp3 : " + beginko.AddMinutes(10).ToString();
+                    msg += Environment.NewLine;
+                    MacthDay.Add(beginko.AddMinutes(10));
+                    msg += "rp4" + " : " + d.ToString();
+                    msg += Environment.NewLine;
+                    MacthDay.Add(d);
+
+                    for (int i = 1; i < 8; i++)
+                    {
+                        msg += "rp" + (i + 4) + " : " + d.AddMinutes(15 * i).ToString();            
+                        msg += Environment.NewLine;
+                        MacthDay.Add(d.AddMinutes(15 * i));
+                    }
+                    curmacth = d;
+                }
+                Console.WriteLine(msg);
+
+                if (_SystemConfig.IsNotifyLine)
+                { 
+                    _APISv.NotiLine(msg); 
+                }
+
+            }
+            var ss =  _APISv.GetArrays();
+            var x = ss.items.Where(a=> _SystemConfig.PureArray.Contains(a.name));
+
+            //var dd = ss.Where
+            _APISv.GetVolumes();
+            //Console.WriteLine(_APISv.Gettoken());
+            Console.ReadLine();
+        }
+
+        private static void LoadConfig()
+        {
+            using (StreamReader r = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "SystemConfig.json"))
+            {
+                string json = r.ReadToEnd();
+                _SystemConfig = JsonConvert.DeserializeObject<SystemConfig>(json);
+            }
+        }
+
+        private static System.Timers.Timer aTimer;
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(6000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            DateTime datenow = DateTime.Today;
+            DateTime date1 = new DateTime(datenow.Year, datenow.Month, datenow.Day, 6, 0, 0);
+            DateTime date2 = new DateTime(datenow.Year, datenow.Month, datenow.Day, 3, 45, 0);
+            var _TimeNow = DateTime.Now;
+
+            if (_TimeNow >= beginko || _TimeNow <= date2)
+            {
+                aTimer = new System.Timers.Timer(1000);
+            }
+            else 
+            { 
+                aTimer = new System.Timers.Timer(6000); 
+            }
+        }
+        public class SystemConfig
+        {
+            public int BeginKickOff { get; set; }
+            public int EveryBeginKickOff { get; set; }
+            public int OnKickOff { get; set; }
+            public int AfterMatchStop { get; set; }
+            public bool IsNotifyLine { get; set; }
+            public bool IsNotifyLineWithImg { get; set; }
+            public List<string> PureArray { get; set; }
+            
+        }
+
+        // 
+    
+
+
+    }
+}
